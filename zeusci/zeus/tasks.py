98f89dc6445e98d5e4b5ad3f800928003d68f90a
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-09-08 23:38:55 +0200

    Removed stupid tasks

--- zeusci/zeus/tasks.py
+++ zeusci/zeus/tasks.py
@@ -1,17 +1,7 @@
 from __future__ import unicode_literals
 from celery import task
 import inspect
-import time
 
-
-@task()
-def add(x, y):
-    return x + y
-
-@task()
-def mul(x, y):
-    time.sleep(3)
-    return x * y
 
 @task
 def do_build_project(project):
