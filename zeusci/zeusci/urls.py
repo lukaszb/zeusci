54a7d5b948ba4cca5c8100ffff8bd5ec7dfc5949
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-09-12 11:03:34 +0200

    Updated requirements

--- zeusci/zeusci/urls.py
+++ zeusci/zeusci/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
-import socketio.sdjango
+#import socketio.sdjango
 
 
 admin.autodiscover()
@@ -19,7 +19,7 @@ home = HomeView.as_view()
 
 
 urlpatterns = patterns('',
-    url("^socket\.io", include(socketio.sdjango.urls)),
+    #url("^socket\.io", include(socketio.sdjango.urls)),
     url(r'^$', home, name='home'),
     url(r'^api/', include('zeus.api.urls')),
     url(r'^', include('zeus.urls')),
