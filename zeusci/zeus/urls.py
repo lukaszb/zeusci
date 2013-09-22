36962522deca206ef6246df58cdc9fd4a7a79859
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-09-23 00:59:00 +0200

    Added comment to force builders urls

--- zeusci/zeus/urls.py
+++ zeusci/zeus/urls.py
@@ -1,10 +1,19 @@
 from __future__ import unicode_literals
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 from smarturls import surl
 
 
 urlpatterns = patterns('zeus.views',
-    surl('/p/<slug:name>/$', 'project_view', name='zeus_project_detail'),
+    # TODO: make helper 'build forcers' proper API endpoints
+    surl('/p/<slug:name>/buildset/$',
+        view='project_buildset_view',
+        name='zeus_force_project_buildset'),
+    surl('/p/force/<slug:name>/builds/<int:buildset_no>\.<int:build_no>/$',
+        view='project_build_view',
+        name='zeus_force_project_build'),
+
+    #surl('/p/<slug:name>/*', 'project_view', name='zeus_project_detail'),
+    url('^p/(?P<name>\w+)/*', 'project_view', name='zeus_project_detail'),
     surl('/p/<slug:name>/buildset/<int:buildset_no>/$',
         view='project_buildset_detail_view',
         name='zeus_project_buildset_detail'),
@@ -12,11 +21,5 @@ urlpatterns = patterns('zeus.views',
         view='project_build_detail_view',
         name='zeus_project_build_detail'),
 
-    surl('/p/<slug:name>/buildset/$',
-        view='project_buildset_view',
-        name='zeus_force_project_buildset'),
-    surl('/p/force/<slug:name>/builds/<int:buildset_no>\.<int:build_no>/$',
-        view='project_build_view',
-        name='zeus_force_project_build'),
 )
 
