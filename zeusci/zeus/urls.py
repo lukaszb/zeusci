ec469827048c06a4db317eb4f3e6f91e2bad7963
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-10-01 23:48:40 +0200

    Fixed urls

--- zeusci/zeus/urls.py
+++ zeusci/zeus/urls.py
@@ -14,15 +14,15 @@ urlpatterns = patterns('zeus.views',
 
     # the same view handles those paths as frontend client is responsible for
     # further routing.
-    url('^p/(?P<name>\w+)/*',
+    url('^p/(?P<name>[-\w]+)/*',
         view='project_view',
         name='zeus_project_detail'),
     # we need buildset/build routes here so the urls can be bookmarked (or page
     # refreshed)
-    url('^p/(?P<name>\w+)/buildsets/(?P<buildset_no>\d+)/$',
+    url('^p/(?P<name>[-\w]+)/buildsets/(?P<buildset_no>\d+)/$',
         view='project_view',
         name='zeus_project_buildset_detail'),
-    url('^p/(?P<name>\w+)/buildsets/(?P<buildset_no>\d+)\.(?P<build_no>\d+)/$',
+    url('^p/(?P<name>[-\w]+)/buildsets/(?P<buildset_no>\d+)\.(?P<build_no>\d+)/$',
         view='project_view',
         name='zeus_project_build_detail'),
 )
