ea115028cca533ec9962e7f4d48fe52b05f4e937
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-11-11 23:53:58 +0100

    zci 'prepare' command

--- zeusci/cli.py
+++ zeusci/cli.py
@@ -3,6 +3,7 @@ from monolith.cli import SingleLabelCommand
 from monolith.cli import BaseCommand
 from monolith.cli import arg
 import os
+import subprocess
 import sys
 import venv
 
@@ -43,16 +44,23 @@ class PrepareCommand(BaseCommand):
 
     def get_python_exec(self):
         # TODO: Let user specify this
-        return '/usr/local/bin/python3.3'
+        #return '/usr/local/bin/python3.3'
+        venv_dir = self.get_venv_dir()
+        return abspath(venv_dir, 'bin', 'python')
 
-    def handle(self, namespace):
+    def get_venv_dir(self):
         ROOT_DIR = get_project_root()
         if ROOT_DIR is None:
             self.error('No zeusci project could be found! Tried to find .zci dir')
 
-        VENV_DIR = abspath(ROOT_DIR, 'venv')
-        self.info("ROOT_DIR = %r" % ROOT_DIR)
-        self.create_venv(namespace, VENV_DIR)
+        return abspath(ROOT_DIR, 'venv')
+
+    def handle(self, namespace):
+        self.info("ROOT_DIR = %r" % get_project_root())
+        #self.create_venv(namespace)
+        #self.install_setuptools(namespace)
+        ##self.install_pip(namespace)
+        self.install_packages(namespace)
 
     def get_builder(self, namespace):
         return venv.EnvBuilder(
@@ -62,11 +70,36 @@ class PrepareCommand(BaseCommand):
             upgrade=False,
         )
 
-    def create_venv(self, namespace, venv_dir):
+    def create_venv(self, namespace):
+        venv_dir = self.get_venv_dir()
+        if os.path.isdir(venv_dir) and not namespace.force:
+            self.info('Virtualenv exists at %s' % venv_dir)
+            return
         self.info("Creating virtualenv at %s" % venv_dir)
         builder = self.get_builder(namespace)
         builder.create(venv_dir)
         self.info('Done')
+
+    def install_setuptools(self, namespace):
+        py_exec = self.get_python_exec()
+        ez_setup = abspath(get_project_root(), 'config', 'ez_setup.py')
+        cmd = ' '.join([py_exec, ez_setup])
+        subprocess.call(cmd, shell=True)
+
+    def install_pip(self, namespace):
+        py_exec = self.get_python_exec()
+        get_pip = abspath(get_project_root(), 'config', 'get-pip.py')
+        cmd = ' '.join([py_exec, get_pip])
+        subprocess.call(cmd, shell=True)
+
+    def install_packages(self, namespace):
+        root_dir = get_project_root()
+        venv_dir = self.get_venv_dir()
+        requirements_path = abspath(root_dir, 'config', 'requirements.txt')
+        pip_exec = abspath(venv_dir, 'bin', 'pip')
+        cmd_args = [pip_exec, 'install', '-r', requirements_path]
+        cmd = ' '.join(cmd_args)
+        subprocess.call(cmd, shell=True)
 
 
 def main():
