8502b247963c38409269a51df9e2c401e38b0fa1
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2014-02-16 00:39:55 +0100

    Implemented breadcrumbs

--- zeusci/zeus/static/zeus/js/app/controllers.js
+++ zeusci/zeus/static/zeus/js/app/controllers.js
@@ -1,3 +1,32 @@
 zeus.controller('ProjectDetailsController', function ($scope, project) {
     $scope.project = project;
 });
+
+
+zeus.controller('BreadcrumbsController', function ($scope, $state, $stateParams) {
+
+    var getBreadcrumbs = function () {
+        var breadcrumbs = [];
+
+        var addBreadcrumb = function (name, url) {
+            breadcrumbs.push({name: name, url: url});
+        };
+
+        if ($stateParams.name) {
+            var url = $state.href('project.details', $stateParams);
+            addBreadcrumb($stateParams.name, url);
+        }
+
+        if ($stateParams.buildsetNumber) {
+            var name = "Buildset #" + $stateParams.buildsetNumber;
+            var url = $state.href('project.details.buildset', $stateParams);
+            addBreadcrumb(name, url);
+        }
+
+        return breadcrumbs;
+    }
+
+    $scope.$on('$stateChangeSuccess', function () {
+        $scope.breadcrumbs = getBreadcrumbs();
+    });
+});
