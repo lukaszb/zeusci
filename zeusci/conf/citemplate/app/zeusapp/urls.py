8a5368b828f51cca1b68bf5dac38262092bf3d7b
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2014-02-18 01:59:50 +0100

    Home page should redirect to project list

--- zeusci/conf/citemplate/app/zeusapp/urls.py
+++ zeusci/conf/citemplate/app/zeusapp/urls.py
@@ -1,19 +1,15 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 
 admin.autodiscover()
 
 
-class HomeView(TemplateView):
-    template_name = 'base.html'
+class HomeView(RedirectView):
+    permanent = False
+    pattern_name = 'zeus_project_list'
 
-    def get_context_data(self, **kwargs):
-        from zeusci.zeus.models import Project
-        data = super(HomeView, self).get_context_data(**kwargs)
-        data['projects'] = Project.objects.all()
-        return data
 
 home = HomeView.as_view()
 
