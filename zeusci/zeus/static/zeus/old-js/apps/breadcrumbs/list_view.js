0292fe3fb8539fe3545a453311dd50b0734dc8e2
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2014-07-23 11:13:49 +0200

    removed obsolate js

--- zeusci/zeus/static/zeus/old-js/apps/breadcrumbs/list_view.js
+++
@@ -1,9 +0,0 @@
-zeus.module("BreadcrumbsApp.List", function (List, App, Backbone, Marionette, $, _) {
-
-
-    List.BreadcrumbsView = App.Views.ItemView.extend({
-        template: "#breadcrumbs-template",
-        contextCollectionName: 'breadcrumbs',
-    });
-
-});
